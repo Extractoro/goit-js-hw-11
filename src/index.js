589b6aa8b3fa362384797d334c6602807b9f9d8d
Vadym Tytarenko (Extractoro) <67538058+Extractoro@users.noreply.github.com>
2022-02-16 20:09:51 +0200

    basic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+import '../node_modules/@fortawesome/fontawesome-free/js/solid.min';
+import '../node_modules/@fortawesome/fontawesome-free/js/fontawesome.min';
+// import './js/fetch_api';
+// import './js/render_cards';
+import './js/arrow_up';
